e8add640b106112446f47047a92a09793a23a5a3
McKenzie Fiege <mckenziefiege@McKenzies-MacBook-Pro.local>
2018-09-10 09:43:01 -0400

    Done

--- solution.rb
+++ solution.rb
@@ -1,7 +1,28 @@
 class Deck
+@@suit = ["Hearts", "Clubs", "Diamonds", "Spades"]
+@@rank = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
+  attr_accessor :cards
+
+  def initialize
+    @cards = []
+    @@suit.each do |suit|
+      @@rank.each do |rank|
+        @cards << Card.new(suit, rank)
+      end
+    end
+  end
+
+  def choose_card
+    @cards.delete(@cards.sample)
+  end
 end
 
 class Card
+  attr_reader :rank, :suit
 
+  def initialize(suit, rank)
+    @rank = rank
+    @suit = suit
+  end
 end
